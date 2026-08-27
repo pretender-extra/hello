e4caf5e4975e5d3b2f82b45953bada82171d99bf
pretender-extra <37943509+pretender-extra@users.noreply.github.com>
2026-08-27 15:11:27 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Ilian Campi
+ * Date:   2026-08-27
+ *
+ * A program that prints my name.
+ */
+
+#include<stdlib.h>
+#include<stdio.h>
+
+
+int main(int argc, char **argv) {
+
+    char *author_name = "Ilian Campi";
+
+    printf("%s\n", author_name);
+
+    return 0;
+}
